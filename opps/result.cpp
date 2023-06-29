382738f4566385db860553fbe8899a4e97470558
Sahil <sahildudhat1919@gmail.com>
2023-06-29 08:10:08 +0530

    29-06-2023

--- opps/result.cpp
+++ opps/result.cpp
@@ -1,37 +1,55 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-class result{
-    int eng,guj,hindi,total;
-    float per;
+class Result{
+    int maths, sci, eng, tot;
+    double per;
+    string grd;
     public:
     void setter(){
-        cout<<"enter english marks :";
+        cout<<"Enter Maths Mark : ";
+        cin>>maths;
+        cout<<"Enter Science Mark : ";
+        cin>>sci;
+        cout<<"Enter English Mark : ";
         cin>>eng;
-        cout<<"enter gujrati marks :";
-        cin>>guj;
-        cout<<"enter hindi marks :";
-        cin>>hindi;
         cal();
     }
     void cal(){
-        total=eng+guj+hindi;
-        per=total/3;
+        tot = maths+sci+eng;
+        per = (double)tot/3;
+        grade();
+    }
+    void grade(){
+        if(maths>34 && sci>34 && eng>34){
+            if(per>=80){
+                grd="Grade A";
+            }else if(per<=80 &&per>65){
+                grd="Grade B";
+            }else if(per<=65 &&per>50){
+                grd="Grade C";
+            }else if(per<=50 &&per>35){
+                grd="Grade D";
+            }
+        }else{
+            grd="Fail...";
+        }
     }
     void getter(){
        /* cout<<"\n";
-        cout<<"value of eng :"<<eng<<endl;
-        cout<<"value of guj :"<<guj<<endl;
-        cout<<"value of hindi :"<<hindi<<endl;
-        cout<<"value of total :"<<t<<endl;
-        cout<<"value of per :"<<setprecision(4)<<per<<endl;*/
+        cout<<"Maths : "<<maths<<endl;
+        cout<<"Science : "<<sci<<endl;
+        cout<<"English : "<<eng<<endl;
+        cout<<"Total : "<<tot<<endl;
+        cout<<"Percentage : "<<setprecision(4)<<per<<endl;*/
         cout<<"\n";
-        cout<<"eng\tguj\thindi\ttotal\tper"<<endl;
-        cout<<eng<<"\t"<<guj<<"\t"<<hindi<<"\t"<<total<<"\t"<<setprecision(4)<<per;
+        cout<<"Maths\tScience\tEnglish\tTotal\tPercentage\tGrade"<<endl;
+        cout<<maths<<"\t"<<sci<<"\t"<<eng<<"\t"<<tot<<"\t"<<setprecision(4)<<per<<"\t\t"<<grd;
+
     }
-}; 
+};
 int main(){
-    result s;
-    s.setter();
-    s.getter();
+    Result n;
+    n.setter();
+    n.getter();
 }
